894f683e0f207f9e3d92b572ec1e074f0c6d049e
apineiroandion <apineiroandion@danielcastelao.org>
2024-10-08 14:50:23 +0200

    enum colores, metodos de logica del juego

--- app/src/main/java/com/example/simondice/MainActivity.kt
+++ app/src/main/java/com/example/simondice/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.simondice
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -8,6 +9,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Arrangement.Absolute.Center
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -16,6 +18,7 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -23,6 +26,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.simondice.ui.theme.Colors
 import com.example.simondice.ui.theme.SimonDiceTheme
 
 class MainActivity : ComponentActivity() {
@@ -40,6 +44,36 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+fun comprobacion (){
+    if (secuencia.size == secuenciaMaquina.size){
+        comprobarSecuencia()
+    }
+}
+fun comprobarSecuencia(){
+    if (secuenciaMaquina == secuencia){
+        secuencia.clear()
+        Log.d("TAG", "CORRECTO")
+
+    }else{
+        secuencia.clear()
+        secuenciaMaquina.clear()
+        Log.d("TAG", "INCORRECTO")
+    }
+}
+val secuenciaMaquina = mutableListOf<Int>()
+fun generarSecuenciaMaquina(){
+    val random = (0..3).random()
+    secuenciaMaquina.add(random)
+    Log.d("TAG", Colors.entries.get(random).nombre + " " + Colors.entries.get(random).id)
+    Log.d("TAG", secuenciaMaquina.toString())
+}
+val secuencia = mutableListOf<Int>()
+fun click(id : Int){
+    secuencia.add(id)
+    Log.d("TAG", Colors.entries.get(id).nombre + " " + Colors.entries.get(id).id)
+    Log.d("TAG", secuencia.toString())
+    comprobacion()
+}
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Column (
@@ -51,53 +85,66 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
             fontSize = 38.sp,
             modifier = Modifier.padding(vertical = 100.dp),
         )
-        Button(onClick = { /*TODO*/ },
-            modifier = Modifier.padding(10.dp)
-                .size(300.dp, 100.dp),
-            colors = ButtonDefaults.buttonColors(
-                containerColor = Color.Red, // Color de fondo del n botón
-                contentColor = Color.White,   // Color del texto del botón
-
-            )
-        ) {
-            Text(text = "RED")
-
+        Row {
+            Column {
+                Button(onClick = { click(Colors.RED.id) },
+                    modifier = Modifier
+                        .padding(10.dp)
+                        .size(150.dp, 100.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color.Red, // Color de fondo del n botón
+                        contentColor = Color.White,   // Color del texto del botón
+                    )
+                ) {
+                    Text(text = Colors.RED.nombre)
+                }
+                Button(onClick = { click(Colors.BLUE.id) },
+                    modifier = Modifier
+                        .padding(10.dp)
+                        .size(150.dp, 100.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color.Blue, // Color de fondo del n botón
+                        contentColor = Color.White,   // Color del texto del botón
+                    )
+                ) {
+                    Text(text = Colors.BLUE.nombre)
+                }
+            }
+            Column {
+                Button(onClick = { click(Colors.GREEN.id) },
+                    modifier = Modifier
+                        .padding(10.dp)
+                        .size(150.dp, 100.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color.Green, // Color de fondo del n botón
+                        contentColor = Color.White,   // Color del texto del botón
+                    )
+                ) {
+                    Text(text = Colors.GREEN.nombre)
+                }
+                Button(onClick = { click(Colors.YELLOW.id) },
+                    modifier = Modifier
+                        .padding(10.dp)
+                        .size(150.dp, 100.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color.Yellow, // Color de fondo del n botón
+                        contentColor = Color.White,   // Color del texto del botón
+                    )
+                ) {
+                    Text(text = Colors.YELLOW.nombre)
+                }
+            }
         }
-        Button(onClick = { /*TODO*/ },
-            modifier = Modifier.padding(10.dp)
+        TextButton(onClick = { generarSecuenciaMaquina() },
+            modifier = Modifier
+                .padding(10.dp)
                 .size(300.dp, 100.dp),
             colors = ButtonDefaults.buttonColors(
-                containerColor = Color.Blue, // Color de fondo del n botón
+                containerColor = Color.Gray, // Color de fondo del n botón
                 contentColor = Color.White,   // Color del texto del botón
-
             )
         ) {
-            Text(text = "BLUE")
-
-        }
-        Button(onClick = { /*TODO*/ },
-            modifier = Modifier.padding(10.dp)
-                .size(300.dp, 100.dp),
-            colors = ButtonDefaults.buttonColors(
-                containerColor = Color.Green, // Color de fondo del n botón
-                contentColor = Color.White,   // Color del texto del botón
-
-            )
-        ) {
-            Text(text = "GREEN")
-
-        }
-        Button(onClick = { /*TODO*/ },
-            modifier = Modifier.padding(10.dp)
-                .size(300.dp, 100.dp),
-            colors = ButtonDefaults.buttonColors(
-                containerColor = Color.Yellow, // Color de fondo del n botón
-                contentColor = Color.White,   // Color del texto del botón
-
-            )
-        ) {
-            Text(text = "YELLOW")
-
+            Text(text = "START ronda: " + secuenciaMaquina.size)
         }
     }
 }
---
+++ app/src/main/java/com/example/simondice/ui/theme/Colors.kt
@@ -0,0 +1,8 @@
+package com.example.simondice.ui.theme
+
+enum class Colors (val id: Int, val nombre: String){
+    RED(0, "Red"),
+    BLUE(1, "Blue"),
+    GREEN(2, "Green"),
+    YELLOW(3, "Yellow")
+}
